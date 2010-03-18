cca78588008841402aaa4b4ca90f5971daecf900
Sergio Lopes <slopes@gmail.com>
2010-03-18 04:18:36 -0300

    simplificando analytics

---
+++ analytics.js
@@ -0,0 +1,8 @@
+$.getScript('http://www.google-analytics.com/ga.js', function(){
+	try {
+		var pageTracker = _gat._getTracker("UA-61051-4");
+		pageTracker._trackPageview();
+	} catch(err) {
+		// ...
+	}	
+});
